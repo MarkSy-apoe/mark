4f1392f7e9165d0bfc5d0b1635c5086dbd9a1eb9
The Hacking Alien <marksyapoe@gmail.com>
2023-12-25 22:24:24 +0100

    page update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,42 +21,44 @@ export default function RootLayout({
   return (
     <html lang="en">      
       
-      <body className='bg-white'>
-        <nav className='w-full h-24 fixed top-0 flex flex-row justify-center items-center backdrop-filter backdrop-blur-lg bg-opacity-20 bg-white text-black'>
+      <body className='bg-white dark:bg-slate-900'>
+        <nav className='w-full h-24 fixed top-0 flex flex-row justify-center items-center backdrop-filter backdrop-blur-lg bg-opacity-20 bg-white dark:bg-slate-900 text-black'>
           
           <div className='w-2/6 md:w-2/6 ml-4'>
-            <h1 className='font-semibold tracking-wide text-2xl text-purple-800 sm:text-3xl sm:text-black md:text-4xl '>Mark <span className='text-purple-800 hidden sm:inline'>Sy-apoe</span></h1>
+            <h1 className='-ml-10 sm:ml-0 font-semibold tracking-wide text-xl dark:text-purple-200 sm:text-3xl sm:text-black dark:sm:text-white md:text-4xl '>Mark <span className='text-purple-800 dark:text-purple-300 text-xl sm:text-3xl md:text-4xl'>Sy-apoe</span></h1>
           </div>
           
-          <div className='md:w-3/6 h-24 flex flex-row justify-center items-center text-black text-lg'>
+          <div className='md:w-3/6 h-24 flex flex-row justify-center items-center text-black dark:text-white text-lg'>
             
-            <p className='hover:text-purple-800 cursor-pointer hidden md:inline'>Home</p>
-            <p className='pl-8 hover:text-purple-800 cursor-pointer hidden md:inline'>About</p>
-            <p className='pl-8 hover:text-purple-800 cursor-pointer hidden md:inline'>Projects</p>
-            <p className='pl-8 hover:text-purple-800 cursor-pointer hidden md:inline'>Blog</p>
+            <Link href='/#home'><p className='hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer hidden md:inline'>Home</p></Link>
+            <Link href='/#about'><p className='pl-8 hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer hidden md:inline'>About</p></Link>
+            <p className='pl-8 hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer hidden md:inline'>Projects</p>
+            <p className='pl-8 hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer hidden md:inline'>Blog</p>
 
           </div>
 
           <div className='w-2/6 md:w-1/6 flex items-center justify-center'>
-            <button className='w-32 hidden h-12 md:inline md:text-md text-black font-semibold border-2 border-black bg-purple-400 rounded-md hover:bg-purple-600 hover:shadow-none nbutton'>Download CV</button>
+            <button className='w-32 hidden h-12 md:inline md:text-md text-black dark:text-white font-semibold border-2 border-black bg-purple-400 rounded-md hover:bg-purple-600 dark:hover:bg-purple-500 hover:shadow-none nbutton'>Download CV</button>
             
-            <svg className="w-6 h-6 cursor-pointer md:hidden ml-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" id='togglebutton'>
+            <svg className="w-6 h-6 cursor-pointer text-black dark:text-white md:hidden ml-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" id='togglebutton'>
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
             
           </div>
          
         </nav>
-        <div className='mt-24 fixed inline md:hidden'>
-          <div className='ml-12 mt-4 w-full h-0 gap-4 grid grid-cols-1 md:hidden text-black overflow-hidden' id='navmobile' style={{transition: 0.3 + 's'}}>
-            <p className='hover:text-purple-800 cursor-pointer'>Home</p>
-            <p className='hover:text-purple-800 cursor-pointer'>About</p>
-            <p className='hover:text-purple-800 cursor-pointer'>Projects</p>
-            <p className='hover:text-purple-800 cursor-pointer'>Blog</p>
-            <button className='w-32 h-12 text-black font-semibold border-2 border-black bg-purple-400 rounded-md  hover:bg-purple-600 hover:shadow-none nbutton'>Download CV</button>
+        <div className='mt-24 w-full fixed inline md:hidden'>
+          <div className='pl-12 w-full h-0 gap-4 bg-white dark:bg-slate-900 grid grid-cols-1 backdrop-filter backdrop-blur-lg bg-opacity-20 text-black dark:text-white overflow-hidden' id='navmobile' style={{transition: 0.3 + 's'}}>
+            <p className='hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer'>Home</p>
+            <p className='hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer'>About</p>
+            <p className='hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer'>Projects</p>
+            <p className='hover:text-purple-800 dark:hover:text-purple-300 cursor-pointer'>Blog</p>
+            <button className='w-32 h-12 text-black dark:text-white font-semibold border-2 border-black bg-purple-400 rounded-md  hover:bg-purple-600 dark:hover:bg-purple-500 hover:shadow-none nbutton'>Download CV</button>
           </div>
+          <Script type='text/javascript' src='/_next/static/js/nav.js'></Script>
         </div>
-        <Script type='text/javascript' src='/_next/static/js/nav.js'></Script>
+        
+        <br></br>
         
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,63 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
-      <p className='mt-28 text-lg'>Hey</p>
+    <main className='mt-44 ml-6 md:ml-12 text-neutral-800 dark:text-white' id='content' style={{transition: 0.3 + 's'}}>
+      <div className='grid xl:grid-cols-2 gap-[23rem]' id='home'>
+        <div>
+          <h1 className='text-xl md:text-4xl font-semibold' id='typewrite'></h1>
+          <p className='mt-8 text-lg md:text-xl md:font-semibold'>Hello, i'm Mark Sy-apoe.<br></br>I'm a certified penetration tester and a fullstack web developer.</p>
+          <div className='grid grid-cols-1 sm:grid-cols-2 w-[17rem] sm:w-[41rem] md:w-[45rem] mt-8 font-semibold gap-5'>
+            <div className='w-[17rem] sm:w-[18rem] md:w-[20rem] break-words md:text-xl text-lg'>
+              <p>A skilled software developer with 5yrs+ experience, with a great affinity for security and secure coding.</p>
+            </div>
+
+            <div className='w-[17rem] sm:w-[18rem] md:w-[20rem] break-words md:text-xl text-lg'>
+              <p>Highly talented penetration tester and exploit developer with 2yrs+ experience.</p>
+            </div>         
+          </div>
+
+          <Link href='/#about'><button className='mt-12 w-52 h-16 bg-purple-400 border-2 border-black rounded-md text-black dark:text-white font-semibold nbutton hover:bg-purple-600 dark:hover:bg-purple-500 hover:shadow-none'>Know more about me</button></Link>       
+        </div>
+
+        <div className='hidden xl:inline'>
+          <div className='w-[20rem] h-[20rem] bg-purple-400 ncard'>
+            <Image
+              src="/mojilike.svg"
+              width={300}
+              height={300}
+              alt='Memoji'
+            />
+          </div>
+        </div>
+      </div>
+
+
+      <div className='mt-32 w-full md:flex md:flex-row grid grid-cols-1' id='about'>
+        <div className='mt-24'>
+          <img src='https://media.giphy.com/media/3ornk57KwDXf81rjWM/giphy.gif' className='w-[30rem] h-[23rem]' alt='Memoji'/>
+        </div>
+
+        <div className='w-full md:w-[40rem] break-words md:ml-12'>
+          <div className='w-32 h-12 bg-purple-400 flex items-center justify-center nbutton mt-4'>
+            <h1 className='text-2xl font-semibold'>About Me</h1>
+          </div>
+          <p className='mt-4 text-lg'>Welcome to my digital realm! I'm Mark, a passionate Fullstack Web Developer with a knack for seamlessly blending creativity and technical expertise.<br></br><br></br> Currently navigating the final level of my Information and Management Technology journey at university, I've honed my skills in crafting robust web solutions, delving into network administration, and exploring the intricate world of penetration testing.<br></br><br></br>
+
+My digital canvas extends beyond coding, as I find joy in crafting captivating digital art that mirrors my imaginative spirit. When not immersed in lines of code, you'll likely find me strategizing and engaging in intense battles in TCG games like Yu-Gi-Oh—a realm where strategy and creativity converge.<br></br><br></br>
+
+In the ever-evolving landscape of technology, I am driven by a relentless curiosity and a passion for continuous learning. Join me on this journey where the digital meets the creative, and together, we'll build immersive and innovative experiences.</p>
+<Link href='/#about'><button className='mt-8 w-52 h-16 bg-purple-400 border-2 border-black rounded-md text-black dark:text-white font-semibold nbutton hover:bg-purple-600 dark:hover:bg-purple-500 hover:shadow-none text-xl'>My Skillset</button></Link>
+        </div>
+      </div>
+
+      <div className='mt-32 -ml-6 md:-ml-12 w-full flex justify-center'>
+       <div className='w-44 h-12 bg-purple-400 flex items-center justify-center nbutton'>
+        <h1 className='text-2xl font-semibold'>My Projects</h1>
+       </div>
+      </div>
+      <br></br><br></br>
     </main>
   )
 }
